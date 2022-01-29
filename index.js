e861b743faaabff7721143b89da118ad9b1b050b
Daniel Soares <danielandradesoares@hotmail.com>
2022-01-29 17:24:26 -0300

    Change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const start = () => {
   //App start
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 3000;
   app.listen(port, () => console.log(`Server started listening on port ${port}!`));
 };
 
